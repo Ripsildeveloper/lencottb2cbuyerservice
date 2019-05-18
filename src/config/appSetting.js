e02caf6ba60efbc48bd05c1a8aaf067b3ee2aca5
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-18 12:18:03 +0530

    ui

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,15 +1,15 @@
 module.exports = {
-  productServerPath: 'http://localhost/Lencott/products/',
+ /*  productServerPath: 'http://localhost/Lencott/products/',
   bannerServerPath: 'http://localhost/Lencott/banners/',
   adsServerPath: 'http://localhost/Lencott/ads/',
   categoryServerPath: 'http://localhost/Lencott/category/',
   logoServerPath: 'http://localhost/Lencott/logo/',
   headerUploadPath: 'images/header',
-  headerServerPath: 'http://localhost/Lencott/header/'
-  /* productServerPath: 'http://lencott.com/admin/images/product/',
+  headerServerPath: 'http://localhost/Lencott/header/' */
+  productServerPath: 'http://lencott.com/admin/images/product/',
   bannerServerPath: 'http://lencott.com/admin/images/banner/',
   adsServerPath: 'http://lencott.com/admin/images/ads/',
   logoServerPath: 'http://lencott.com/admin/images/logo/',
-  headerServerPath: 'http://lencott.com/admin/images/header/' */
+  headerServerPath: 'http://lencott.com/admin/images/header/'
 };
 
--- src/model/cart.model.js
+++ src/model/cart.model.js
@@ -3,7 +3,7 @@ var Size = require('./size.model');
 
 const cartSchema = new mongoose.Schema({
     userId: String,
-    items: [{productId: mongoose.Schema.Types.ObjectId, skuCode: String}]
+    items: [{productId: mongoose.Schema.Types.ObjectId, skuCode: String, qty: String}]
   });
 
   const Cart = mongoose.model('cart', cartSchema);
